3c7c758645359a72c2b59c7afc477aebaefe1e5f
Priscilla Van Dyke <prisvd77@gmail.com>
2021-12-03 10:27:13 -0500

    add legend

---
+++ Earthquake_Challenge/static/js/logicStep5.js
@@ -0,0 +1,124 @@
+// We creat the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We creat the dark view tile layer that will be an option for our map.
+let satellitestreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: API_KEY
+    });
+
+//Creat a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satellitestreets
+};
+
+//create the earthquake layer for our map
+let earthquakes = new L.layerGroup();
+
+//define an object that contians oveerlays (visisble at all times)
+let overlays = {
+    Earthquakes: earthquakes
+};
+// Create the map objects with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets],
+});
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps, overlays).addTo(map);
+//ACCESSING THE AIRPORT geoJSON
+let earthquakeSum = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+
+// get radius of marker (radius of zero willbe plotted as 1)
+function getRadius(magnitude) {
+    if (magnitude == 0) {
+        return 1;
+    }
+    return magnitude * 4;
+};
+// determine color of circle based on magnitude
+function getColor(magnitude) {
+    if (magnitude>5) {
+        return "#ea2c2c";
+    }
+    if (magnitude>4){
+        return "ea822c"
+    }
+    if (magnitude>3) {
+        return "#ee9c00"
+    }
+    if (magnitude > 2) {
+        return "#eecc00";
+      }
+      if (magnitude > 1) {
+        return "#d4ee00";
+      }
+      return "#98ee00";
+    }
+
+// returns style data for each of the earthquakes the map
+function styleInfo(feature) {
+    return {
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    }
+
+};  
+
+d3.json(earthquakeSum).then(function(data) {
+    console.log(data); 
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        onEachFeature: function (feature,layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+        
+    }).addTo(earthquakes);
+    earthquakes.addTo(map);
+
+let legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend"); 
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+// Looping through our intervals to generate a label with a colored square for each interval.
+for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + getColor(colors[i]) + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+ }
+  return div;
+};
+
+legend.addTo(map);
+
+// then we add the earthquake layer to our map.
+
+});
